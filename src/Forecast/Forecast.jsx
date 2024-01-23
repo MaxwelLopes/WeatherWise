bde4a8dee683c1555fa5609fe6df96cda549b5b9
teste <maxwel.batalha@gmail.com>
2024-01-22 21:21:48 -0300

    Adicionar modelo de prever os dias futuros

---
+++ src/Forecast/Forecast.jsx
@@ -0,0 +1,17 @@
+import './Forecast.css'
+
+function Forecast(){
+    return (
+        <>
+            <div className="forecast">
+                <img src="./src/assets/svg/day.svg" alt="" className="svg-current"/>
+                <div className="temperature">
+                    <p className="value">40<span className='scale'>°</span></p>
+                    <p className='differential'>↑32° ↓12°</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Forecast
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,31 @@
 import CurrentWeather from '../currentWeather/CurrentWeather'
 import Search from '../search/Search'
+import Forecast from '../Forecast/Forecast'
 import './App.css'
 
 function App() {
   return (
     <>
       <div className='container'>
+        
         <Search/>
+        
         <CurrentWeather/>
+
+        <div className="forecast-day">
+          <Forecast/>
+          <Forecast/>
+          <Forecast/>
+          <Forecast/>
+        </div>
+
+        <div className="forecast-day">
+          <Forecast/>
+          <Forecast/>
+          <Forecast/>
+          <Forecast/>
+        </div>
+
       </div>
     </>
   )
