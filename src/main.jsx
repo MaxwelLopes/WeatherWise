47e2c639889587aff6d5b2d72d71517b56aa7ffd
teste <maxwel.batalha@gmail.com>
2024-01-24 22:48:54 -0300

    teste .env

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import './index.css'
 
+process.env.API_KEY
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
@@ -10,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 )
 
 
-//9d9cca1fb3c6c5525bb123e10751123b
+
