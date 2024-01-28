b7f64e7c46a2360410e56b1b1bd064a03d4005fa
teste <maxwel.batalha@gmail.com>
2024-01-28 20:57:24 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App'
+import App from './app/App.jsx'
 import './index.css'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
