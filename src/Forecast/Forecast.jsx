69e20036426b4149b2fe988178e2209508f424cb
Max <maxwel.batalha@gmail.com>
2024-03-28 16:29:13 -0300

    melhorias no gráfico

--- src/Forecast/Forecast.jsx
+++ src/Forecast/Forecast.jsx
@@ -4,96 +4,82 @@ import Chart from 'chart.js/auto';
 
 function Forecast({ forecastData }) {
   const chartRef = useRef(null);
-  const [dataLoaded, setDataLoaded] = useState(false);
-  const [chartInstance, setChartInstance] = useState(null); // Armazenar a instância do gráfico
+  const [chartInstance, setChartInstance] = useState(null);
 
   useEffect(() => {
     if (forecastData && forecastData.length > 0 && chartRef.current) {
-      const ctx = chartRef.current.getContext('2d');
-    
       if (chartInstance) {
         chartInstance.destroy();
       }
-    
-      // Criar um novo gráfico
+
+      const ctx = chartRef.current.getContext('2d');
+
       const newChartInstance = new Chart(ctx, {
-        type: 'bar', // Tipo padrão definido como barra para permitir gráficos mistos
+        type: 'bar',
         data: {
           labels: forecastData.map(previsao => previsao[0]),
           datasets: [{
+            label: 'Rain',
+            data: forecastData.map(previsao => previsao[2]),
+            backgroundColor: 'blue',
+            borderColor: 'blue',
+            borderWidth: 1,
+            yAxisID: 'y'
+          },
+          {
             label: 'Temperature',
             data: forecastData.map(previsao => previsao[1]),
             borderColor: 'red',
-            pointStyle: 'circle',
-            type: 'line', 
-          },
-          {
-            label: 'Rain',
-            data: forecastData.map(previsao => previsao[2]),
-            backgroundColor: 'blue', 
-            borderColor: 'blue',
-            pointStyle: 'circle',
-            pointBackgroundColor: 'blue', // Cor do ponto na legenda
-            pointBorderColor: 'blue'
+            borderWidth: 2,
+            type: 'line',
+            fill: false,
+            yAxisID: 'y'
           }]
         },
         options: {
           scales: {
-            x: {
-              grid: {
-                drawOnChartArea: false, 
-              },
-              ticks: { 
-                color: 'aliceblue',
-              }
-            },
             y: {
               beginAtZero: true,
-              ticks: { 
-                color: 'aliceblue',
+              position: 'left',
+              title: {
+                display: true,
+                text: 'Temperature'
+              }
+            },
+            y1: {
+              beginAtZero: true,
+              position: 'right',
+              title: {
+                display: true,
+                text: 'Rain'
+              },
+              grid: {
+                drawOnChartArea: false
+              }
+            },
+            x: {
+              grid: {
+                drawOnChartArea: false
               }
             }
           },
           plugins: {
             legend: {
-              display: true,
-              labels: {
-                usePointStyle: true, 
-                pointStyle: 'line', 
-                color: 'aliceblue' 
-              },
-            },
-            datalabels: {
-              anchor: 'end',
-              align: 'top',
-              formatter: (value) => `${value} mm`
+              display: true
             }
-          },
+          }
         }
       });
-      
-    
-      setChartInstance(newChartInstance); // Armazenar a nova instância do gráfico
-      setDataLoaded(true); // Indica que os dados foram carregados
+
+      setChartInstance(newChartInstance);
     }
   }, [forecastData]);
 
-  // Renderização condicional com base no carregamento dos dados
-  if (!forecastData) {
-    return (
-      <div className="loading-container">
-        <div className="loading-spinner"></div>
-        Carregando...
-      </div>
-    );
-  }
-
   return (
     <div className="forecast">
-      <canvas ref={chartRef} id="temperatureChart"></canvas>
+      <canvas ref={chartRef}></canvas>
     </div>
   );
 }
 
 export default Forecast;
-
