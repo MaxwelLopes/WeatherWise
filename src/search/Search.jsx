1311dc4aa6539e788e6c879a19e86cbf2c48df01
Max <maxwel.batalha@gmail.com>
2024-03-28 16:18:13 -0300

    Implementar autocomplete google

--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './search.css';
 
-function Search({ callFetchData}) {
-  const [city, setCity] = useState(''); // Estado para armazenar a cidade digitada pelo usuário
+function Search({ callFetchData }) {
+  const [city, setCity] = useState('');
+
+  useEffect(() => {
+    if (!window.google) {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_KEY_GOOGLE}&libraries=places`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      script.onload = () => {
+        initializeAutocomplete();
+      };
+
+      // Cleanup function to prevent memory leaks
+      return () => {
+        document.head.removeChild(script);
+      };
+    } else {
+      initializeAutocomplete();
+    }
+  }, [callFetchData]);
+
+  const initializeAutocomplete = () => {
+    const autocomplete = new window.google.maps.places.Autocomplete(
+      document.getElementById('search_input'),
+      { types: ['(cities)'] }
+    );
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        callFetchData(place.name);
+      }
+    });
+  };
 
   const handleChange = (event) => {
-    // Atualiza o estado da cidade conforme o usuário digita no input
     setCity(event.target.value);
   };
 
   const handleClick = () => {
-    // Verifica se o input não está vazio antes de chamar handleFetchData
     if (city.trim() !== '') {
       callFetchData(city);
     }
@@ -24,7 +55,14 @@ function Search({ callFetchData}) {
 
   return (
     <div className="search-container">
-      <input type="text" placeholder="Digite a cidade" value={city} onChange={handleChange} onKeyDown={handleKeyDown}/>
+      <input
+        type="text"
+        id="search_input"
+        placeholder="Digite a cidade"
+        value={city}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
       <button className="search-button" onClick={handleClick} disabled={city.trim() === ''}>
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="var(--text)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
